224dd70d1be1ec0fb1683a0247c6e5263029c7f5
Laura <lauramjohnson@me.com>
2022-05-07 22:07:50 -0500

    setup and launch screen

---
+++ Slot Machine/Utils/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Slot Machine
+//
+//  Created by Laura Johnson on 5/7/22.
+//
+
+import SwiftUI
+
+struct Constants {
+    struct Colors {
+        static let pink = Color("ColorPink")
+        static let yellow = Color("ColorYellow")
+        static let purple = Color("ColorPurple")
+        static let transparentBlack = Color("ColorBlackTransparent")
+    }
+}
+
---
+++ Slot Machine/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Slot Machine
+//
+//  Created by Laura Johnson on 5/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .foregroundColor(Constants.Colors.purple)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
